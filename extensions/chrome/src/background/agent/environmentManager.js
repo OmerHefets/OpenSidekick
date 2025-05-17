daefa4a564ceb9d863b7d779c81265a96eea51e5
OmerHefets <hefets.omer@gmail.com>
2025-05-17 12:24:02 +0300

    remove redundant error mechanism

--- extensions/chrome/src/background/agent/environmentManager.js
+++ extensions/chrome/src/background/agent/environmentManager.js
@@ -50,7 +50,6 @@ export class EnvironmentManager {
         } catch (error) {
             console.error("[EnvironmentManager] Screenshot error:", error);
             this.communications.errorMessage();
-            throw error;
         }
     }
 
